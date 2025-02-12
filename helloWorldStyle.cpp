53ed9864fc92dc955cbee734e471f037faaece6f
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-02-12 19:14:45 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2025 Jayden Smith All rights reserved.
+// Created by: Jayden Smith
+// Date: February 12, 2025
+// Hello World with style in C++.
+
+#include<iostream>
+int main() {
+    std::cout << "Hello World with style!\n";
+    std::cout << "C++ is different then Python\n";
+}
